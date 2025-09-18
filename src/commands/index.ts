380e9b6505abda9526d557ad094aac61aa0cc8fb
RushilK7 <rushilk@lambdatest.com>
2025-09-18 13:37:41 +0530

    Repository cleanup and production readiness - v1.16.0

--- src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all commands
-export { default as Help } from './help';
-export { default as Version } from './version';
-export { default as Update } from './update';
-export { default as Uninstall } from './uninstall';
-export { default as Init } from './init';
-export { default as Migrate } from './migrate';
--- tests/fixtures/multi-platform-demo/applitools.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: process.env.APPLITOOLS_API_KEY,
-  batchName: 'Multi Platform Demo',
-  batchId: process.env.APPLITOOLS_BATCH_ID,
-  browsers: [
-    { width: 1200, height: 800, name: 'chrome' },
-    { width: 768, height: 1024, name: 'chrome' }
-  ]
-};
